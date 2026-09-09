f2002fb628bd279cd7e69db53a89f0e4bcfe8bf3
blackpotato <khoa992006@gmail.com>
2026-09-09 10:36:14 +0700

    feat: add back asset

--- Assets/Scripts/Entity/Player/BodySegmentPool.cs
+++ Assets/Scripts/Entity/Player/BodySegmentPool.cs
@@ -6,21 +6,6 @@ public class BodySegmentPool : ObjectPoolBase<BodySegment>
 
     protected override BodySegment CreateItem()
     {
-        Transform targetParent = bodySegmentParent != null ? bodySegmentParent : transform;
-        BodySegment segmentComponent;
-        if (prefab != null)
-        {
-            segmentComponent = Instantiate(prefab, targetParent);
-        }
-        else
-        {
-            GameObject segment = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            segment.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-            segment.transform.SetParent(targetParent);
-            Collider col = segment.GetComponent<Collider>();
-            if (col != null) Destroy(col);
-            segmentComponent = segment.AddComponent<BodySegment>();
-        }
-        return segmentComponent;
+        return Instantiate(prefab, bodySegmentParent != null ? bodySegmentParent : transform);
     }
 }
